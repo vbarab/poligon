2ad9613ab282742e92eecbe2779add7acb121948
vbara <vbarabancev@gmail.com>
2019-03-20 16:41:16 +0300

    several tasks

--- src/main/java/helloworld/Start.java
+++ src/main/java/helloworld/Start.java
@@ -1,17 +1,17 @@
 package helloworld;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Start {
 
     public static void main(String[] args) throws Exception {
-        testArrayMinimalSearchLambda.main();
+        task34_2.main(args);
     }
 
     private static class task1 {
@@ -47,7 +47,7 @@ public class Start {
             System.out.println(sumDigitsInNumber(546));
         }
 
-        public static int sumDigitsInNumber(int number) {
+        static int sumDigitsInNumber(int number) {
             String tempString = String.valueOf(number);
             char[] split = tempString.toCharArray();
             int result = 0;
@@ -424,5 +424,375 @@ public class Start {
         }
 
     }
+
+    private static class task22 {
+        public static void main(String[] args) throws Exception {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            int arrLenght = Integer.parseInt(reader.readLine());
+            if (arrLenght > 0) {
+                List<Integer> list = new ArrayList();
+                for (int i = 0; i < arrLenght; i++) {
+                    int temp = Integer.parseInt(reader.readLine());
+                    list.add(temp);
+                }
+                int maximal = list
+                        .stream()
+                        .mapToInt(value -> value)
+                        .max()
+                        .orElseThrow(Exception::new);
+                System.out.println(maximal);
+            } else {
+                System.out.println("Число меньше 0");
+            }
+            //напишите тут ваш код
+        }
+    }
+
+    private static class task23 {
+        public static void main(String[] args) throws Exception {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            int sum = 0;
+            while (true) {
+                String temp = bufferedReader.readLine();
+                if (temp.equals("сумма")) {
+                    break;
+                }
+                sum += Integer.parseInt(temp);
+
+                //напишите тут ваш код
+            }
+            System.out.println(sum);
+        }
+    }
+
+    private static class task24 {
+
+        public static int even;
+        public static int odd;
+
+        public static void main(String[] args) throws IOException {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            int nubmer = Integer.parseInt(bufferedReader.readLine());
+            List<Integer> integerList = splitter(nubmer);
+            for (Integer item : integerList) {
+                if (item % 2 != 0) {
+                    odd++;
+                }
+                if (item % 2 == 0) {
+                    even++;
+                }
+            }
+            System.out.println("odd: " + odd + " even: " + even);
+        }
+
+        private static List<Integer> splitter(int a) {
+            List<Integer> list = new ArrayList();
+            while (a > 0) {
+                list.add(a % 10);
+                a = a / 10;
+            }
+            return list;
+        }
+    }
+
+    private static class task25 {
+        static class Cat {
+            static int count;
+
+            public Cat() {
+                count++;
+            }
+        }
+
+        public static void main(String[] args) {
+            List<Cat> catList = new ArrayList<>();
+            for (int a = 0; a < 10; a++) {
+                catList.add(new Cat());
+            }
+            System.out.println(Cat.count);
+
+        }
+    }
+
+    private static class task26 {
+        public static void main(String[] args) throws IOException {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            String grandpaName = bufferedReader.readLine();
+            String grandmaName = bufferedReader.readLine();
+            String papaName = bufferedReader.readLine();
+            String mamaName = bufferedReader.readLine();
+            String daughterName = bufferedReader.readLine();
+            String sonName = bufferedReader.readLine();
+
+            Cat grandpa = new Cat(grandpaName);
+            Cat grandma = new Cat(grandmaName);
+            Cat papa = new Cat(papaName, grandpa, null);
+            Cat mama = new Cat(mamaName, null, grandma);
+            Cat son = new Cat(sonName, papa, mama);
+            Cat daughter = new Cat(daughterName, papa, mama);
+
+            System.out.println(grandpa.toString());
+            System.out.println(grandma.toString());
+            System.out.println(papa.toString());
+            System.out.println(mama.toString());
+            System.out.println(daughter.toString());
+            System.out.println(son.toString());
+        }
+
+        public static class Cat {
+            private String name;
+            private Cat parent;
+            private Cat father;
+            private Cat mother;
+
+            Cat(String name, Cat father, Cat mother) {
+                this.father = father;
+                this.mother = mother;
+                this.name = name;
+            }
+
+
+            Cat(String name) {
+                this.name = name;
+            }
+
+            Cat(String name, Cat parent) {
+                this.name = name;
+                this.parent = parent;
+            }
+
+            @Override
+            public String toString() {
+                if (father == null && mother == null)
+                    return "The cat's name is " + name + ", no mother, no father ";
+                if (father == null)
+                    return "The cat's name is " + name + ", mother is " + mother + ", no father";
+                if (mother == null)
+                    return "The cat's name is " + name + ", no mother" + ", father is " + father;
+                else return "The cat's name is " + name + ",mother is " + mother + ", father is " + father;
+            }
+
+        }
+
+    }
+
+    private static class task27 {
+        public static void main(String[] args) throws Exception {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < 5; i++) {
+                list.add(Integer.parseInt(bufferedReader.readLine()));
+            }
+            Collections.sort(list);
+            for (Integer aList : list) {
+                System.out.println(aList);
+            }
+
+        }
+    }
+
+    private static class task28 {
+        public static void main(String[] args) throws Exception {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            String[] array = new String[10];
+            for (int i = 0; i < 8; i++) {
+                array[i] = bufferedReader.readLine();
+            }
+            for (int i = array.length; i >= 1; i--) {
+                System.out.println(array[i - 1]);
+            }
+
+        }
+    }
+
+    private static class task29 {
+        public static void main(String[] args) throws Exception {
+            //напишите тут ваш код
+            int[] array = new int[10];
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            for (int i = 0; i < array.length; i++) {
+                array[i] = Integer.parseInt(bufferedReader.readLine());
+            }
+            for (int i = array.length - 1; i > -1; i--) {
+                System.out.println(array[i]);
+            }
+        }
+    }
+
+    private static class task30Compare {
+        public static void main(String[] args) throws Exception {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            ArrayList<String> arrayList = new ArrayList<>();
+          /*  Comparator<String> compareLenght = new Comparator<String>() { //вариант базовый
+                @Override
+                public int compare(String o1, String o2) {
+                    return Integer.compare(o1.length(),o2.length());
+                }
+            };*/
+            //   Comparator<String> compareLenght = (o1, o2) -> Integer.compare(o1.length(),o2.length()); //вариант с лямбдой
+            Comparator<String> compareLenght = Comparator.comparingInt(String::length);//вариант compareInt
+            for (int i = 0; i < 5; i++) {
+                arrayList.add(bufferedReader.readLine());
+            }
+            arrayList.sort(compareLenght);
+            for (int i = 0; i < 5; i++) {
+                System.out.println(arrayList.get(i));
+            }
+        }
+    }
+
+    private static class task31CompareMax {
+        public static void main(String[] args) throws Exception {
+            //напишите тут ваш код
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            // ArrayList<String> arrayList = new ArrayList<>();
+            ArrayList<String> strings = new ArrayList<>();
+            Comparator<String> compareMax = (o1, o2) -> Integer.compare(o1.length(), o2.length());
+            for (int i = 0; i < 5; i++) {
+                strings.add(bufferedReader.readLine());
+            }
+            strings.sort(compareMax);
+            Collections.reverse(strings);
+            String temp = strings.get(0);
+            System.out.println(temp);
+            for (String s : strings) {
+                if (s.length() == temp.length()) {
+                    System.out.println(s);
+                }
+
+            }
+        }
+    }
+
+    private static class task32 {
+        public static void main(String[] args) throws Exception {
+            //напишите тут ваш код
+            List<String> list = new ArrayList<>();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            String min = String.valueOf(Math.PI);
+            String max = "";
+            for (int i = 0; i < 6; i++) {
+                list.add(bufferedReader.readLine());
+                if (list.get(i).length() < min.length()) {
+                    min = list.get(i);
+                }
+                if (list.get(i).length() > max.length()) {
+                    max = list.get(i);
+                }
+            }
+            if (list.indexOf(min) < list.indexOf(max)) {
+                System.out.println(min);
+            } else System.out.println(max);
+
+        }
+    }
+
+    private static class task33 {
+        public static void main(String[] args) throws Exception {
+            // Считать строки с консоли и объявить ArrayList list тут
+            ArrayList<String> list = new ArrayList<>();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+            for (int i = 0; i < 4; i++) {
+                list.add(i, bufferedReader.readLine());
+            }
+
+            ArrayList<String> result = doubleValues(list);
+            for (String s : result
+                    ) {
+                System.out.println(s);
+
+            }
+            // Вывести на экран result
+        }
+
+        public static ArrayList<String> doubleValues(ArrayList<String> list) {
+            //напишите тут ваш код
+            for (int i = 0; i < list.size(); i += 2) {
+                list.add(i, list.get(i));
+            }
+
+            return list;
+        }
+    }
+
+    private static class task34 {
+        public static void main(String[] args) throws Exception {
+            ArrayList<String> list = new ArrayList<>();
+            list.add("роза"); // 0
+            list.add("лоза"); // 1
+            list.add("лира"); // 2
+            list.add("жопка");
+            list.add("клерк");
+            list.add("роман");
+            list.add("ламинат");
+            list = fix(list);
+
+            for (String s : list) {
+                System.out.println(s);
+            }
+
+        }
+
+        public static ArrayList<String> fix(ArrayList<String> list) {
+            for (int i = 0; i < list.size(); i++) {
+                if (StringUtils.containsAny(list.get(i), "л")) {
+                    if (!StringUtils.containsAny(list.get(i), "р")) {
+                        list.add(i, list.get(i));
+                    }
+                } else if (StringUtils.containsAny(list.get(i), "р") && !StringUtils.containsAny(list.get(i), "л")) {
+                    list.remove(list.get(i));
+                } else if (StringUtils.containsAny(list.get(i), "р") && StringUtils.containsAny(list.get(i), "л")) {
+                    list.add(list.get(i));
+                }
+            }
+            return list;
+        }
+    }
+
+    //TODO ПОПРАКТИКОАТЬСЯ С ЛЯМБДАМИ
+    private static class task34_2 {
+        public static void main(String[] args) throws Exception {
+            ArrayList<String> list = new ArrayList<>();
+            list.add("роза"); // 0
+            list.add("лоза"); // 1
+            list.add("лира"); // 2
+            list.add("каппа");
+            list.add("морж");
+            list.add("лакомство");
+            list = fix(list);
+
+            for (String s : list) {
+                System.out.println(s);
+            }
+        }
+
+        public static ArrayList<String> fix(ArrayList<String> list) {
+/*            for (int i =list.size()-1;i>0) {
+                if (list.get(i).contains("р")) {
+                    if (list.get(i).contains("л")) {
+
+                    } else if (!list.get(i).contains("л")) {
+                        list.remove(list.get(i));
+                    }
+                }
+                if (list.get(i).contains("л")) {
+                    if (list.get(i).contains("р")) {
+
+                    } else if (!list.get(i).contains("р")) {
+                        list.add(list.get(i));
+                    }
+                }
+
+            }*/
+            list.removeIf(s -> (s.contains("р")) && (!s.contains("л")));
+            list.addAll(list.stream()
+                    .filter(s -> (s.contains("л")) && (!s.contains("р")))
+                    .collect(Collectors.toList()));
+            //напишите тут ваш код
+            return list;
+        }
+    }
+
 }
 
